5d3cc24048918715ab17b95af4ef6e3816514be7
nithinpratap306-stack <nithinpratap306@gmail.com>
2026-08-28 16:01:24 +0530

    Add LC 605 - Can Place Flowers

---
+++ Arrays/605_Can_Place_Flowers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int cnt=0;
+        for(int i=0;i<flowerbed.size();i++){
+            if(flowerbed[i]==0 ){
+                bool lt=(i==0 || flowerbed[i-1]==0);
+                bool rt=(i==flowerbed.size()-1 || flowerbed[i+1]==0);
+
+                if(lt&&rt){
+                    flowerbed[i]=1;
+                    cnt++;
+                }
+                if(cnt>=n) return true;
+            }
+        }
+        return cnt>=n;
+    }
+};
